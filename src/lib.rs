56077ebe1e9dd2d49efb5707ac50afdf8c4024e5
Vee Satayamas <vsatayamas@gmail.com>
2017-10-19 14:50:33 +0700

    generalize prefix tree and bump version to 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,30 @@
 use std::collections::HashMap;
+use std::hash::Hash;
+use std::cmp::Eq;
 
 #[derive(Debug, Clone)]
-pub struct PrefixTree<T> {
-    tab: HashMap<(u32,u32,char),(u32,bool,Option<T>)>
+pub struct PrefixTree<Elem,Payload> where Elem: Hash + Eq + Clone {
+    tab: HashMap<(u32,u32,Elem),(u32,bool,Option<Payload>)>
 }
 
-impl<T: Clone> PrefixTree<T> {
-    pub fn new(sorted_word_list_with_payload: &[(&str,T)]) -> PrefixTree<T> {
-        let mut tab: HashMap<(u32,u32,char),(u32,bool,Option<T>)> = HashMap::new();
+impl<Elem: Hash+Eq+Clone, Payload: Clone> PrefixTree<Elem, Payload> {
+    
+    pub fn new(sorted_word_list_with_payload: &[(Vec<Elem>, Payload)])
+               -> PrefixTree<Elem, Payload>
+    {
+        let mut tab: HashMap<(u32,u32,Elem),(u32,bool,Option<Payload>)> = HashMap::new();
         let word_list_len = sorted_word_list_with_payload.len();
         for i in 0..word_list_len {
-            let (ref word, ref payload) = sorted_word_list_with_payload[i];
-            let char_vec: Vec<char> = word.chars().collect();
-            let word_len = char_vec.len();
+            let (ref item, ref payload) = sorted_word_list_with_payload[i];
+            let item_len = item.len();
             
             let mut row_no: u32 = 0;
-            for j in 0..word_len {
-                let is_terminal = j + 1 == word_len;
-                let ch = char_vec[j];
-                let child = tab.entry((row_no as u32,j as u32,ch))
+            let mut j = 0;
+            
+            for elem in item.into_iter() {
+                let elem = elem.clone();
+                let is_terminal = j + 1 == item_len;
+                let child = tab.entry((row_no as u32,j as u32, elem))
                     .or_insert((i as u32, is_terminal,
                                 if is_terminal {
                                     Some(payload.clone())
@@ -27,12 +33,25 @@ impl<T: Clone> PrefixTree<T> {
                                 }));
                 let &mut (_row_no, _, _) = child;
                 row_no = _row_no;
+                j += 1;
             }            
         }
         PrefixTree{tab: tab}
     }
 
-    pub fn seek(&self, key: &(u32,u32,char)) -> Option<&(u32,bool,Option<T>)> {
+    pub fn seek(&self, key: &(u32,u32,Elem)) -> Option<&(u32,bool,Option<Payload>)> {
         self.tab.get(key)
     }
+}
+
+pub fn prefix_tree_from_str<P:Clone>(sorted_word_list_with_payload: &[(&str, P)])
+                                     -> PrefixTree<char, P>
+{
+    let sorted_word_list_with_payload: Vec<(Vec<char>,P)> =
+        sorted_word_list_with_payload
+        .into_iter()
+        .map(|&(ref item, ref payload)|
+             (item.chars().collect::<Vec<char>>(), payload.clone()))
+        .collect();
+    PrefixTree::new(&sorted_word_list_with_payload[..])
 }
--- tests/seek.rs
+++ tests/seek.rs
@@ -2,20 +2,21 @@ extern crate prefixtree;
     
 #[cfg(test)]
 mod tests {
-    use prefixtree::PrefixTree;
+    use prefixtree::{PrefixTree, prefix_tree_from_str};
 
     #[test]
     fn one_char_test() {
-        let sorted_word_list = vec![("A", 1)];
-        let prefix_tree = PrefixTree::new(&sorted_word_list[..]);
+        let sorted_word_list: Vec<(&str, i32)> = vec![("A", 1)];
+        let prefix_tree = prefix_tree_from_str(&sorted_word_list[..]);
         assert!(prefix_tree.seek(&(0,0,'A')) == Some(&(0, true, Some(1))));
         assert!(prefix_tree.seek(&(0,0,'B')).is_none());
     }
 
+    
     #[test]
     fn one_word_test() {
         let sorted_word_list = vec![("AB", 1)];
-        let prefix_tree = PrefixTree::new(&sorted_word_list[..]);
+        let prefix_tree = prefix_tree_from_str(&sorted_word_list[..]);
         assert!(prefix_tree.seek(&(0,0,'A')) == Some(&(0,false,None)));
         assert!(prefix_tree.seek(&(0,1,'B')) == Some(&(0,true,Some(1))));
     }
@@ -23,7 +24,7 @@ mod tests {
     #[test]
     fn two_word_test() {
         let sorted_word_list = vec![("A",1), ("AB",2)];
-        let prefix_tree = PrefixTree::new(&sorted_word_list[..]);
+        let prefix_tree = prefix_tree_from_str(&sorted_word_list[..]);
         assert!(prefix_tree.seek(&(0,0,'A')) == Some(&(0,true,Some(1))));
         assert!(prefix_tree.seek(&(0,1,'B')) == Some(&(1,true,Some(2))));
     }
@@ -31,11 +32,37 @@ mod tests {
     #[test]
     fn three_word_test() {
         let sorted_word_list = vec![("A",1), ("AB",2), ("CXX",3)];
-        let prefix_tree = PrefixTree::new(&sorted_word_list[..]);
+        let prefix_tree = prefix_tree_from_str(&sorted_word_list[..]);
         assert!(prefix_tree.seek(&(0,0,'A')) == Some(&(0,true,Some(1))));
         assert!(prefix_tree.seek(&(0,1,'B')) == Some(&(1,true,Some(2))));
         assert!(prefix_tree.seek(&(0,0,'C')) == Some(&(2,false,None)));
         assert!(prefix_tree.seek(&(2,1,'X')) == Some(&(2,false,None)));
         assert!(prefix_tree.seek(&(2,2,'X')) == Some(&(2,true,Some(3))));
     }
+
+    #[test]
+    fn vec_of_vec_test() {
+        let sorted_list = vec![
+            (vec![1,2], 1),
+            (vec![1,3], 2)
+        ];
+        let prefix_tree = PrefixTree::new(&sorted_list);
+        assert!(prefix_tree.seek(&(0,0,1)) == Some(&(0,false,None)));
+        assert!(prefix_tree.seek(&(0,1,2)) == Some(&(0,true,Some(1))));
+        assert!(prefix_tree.seek(&(0,1,3)) == Some(&(1,true,Some(2))));
+    }
+
+    #[test]
+    fn vec_of_str_vec_test() {
+        let sorted_list = vec![
+            (vec!["CAT", "DOG"], 1),
+            (vec!["CAT", "ZOO"], 2)
+        ];
+        let prefix_tree = PrefixTree::new(&sorted_list);
+        assert!(prefix_tree.seek(&(0,0,"CAT")) == Some(&(0,false,None)));
+        assert!(prefix_tree.seek(&(0,1,"DOG")) == Some(&(0,true,Some(1))));
+        assert!(prefix_tree.seek(&(0,1,"ZOO")) == Some(&(1,true,Some(2))));
+
+    }
+    
 }
